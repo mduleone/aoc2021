fbb3ae074735e005154aacba64f5f906e5dedc01
Matt DuLeone <matt@duleone.com>
2021-12-07 22:25:52 -0500

    Tidy day 7

--- day7.js
+++ day7.js
@@ -8,11 +8,17 @@ const parseData = (input) => {
 }
 
 const parsedData = parseData(data);
+const parsedTest = parseData(test);
 
 const sumOneToN = num => (num * (num + 1) / 2);
 
-const determineFuelConsumptionByIdx = (data, cell, static) => {
-  return data.map(x => static ? Math.abs(x - cell) : sumOneToN(Math.abs(x - cell))).reduce((sum, x) => sum + x, 0);
+const determineFuelConsumptionByIdx = (input, cell, static) => {
+  
+  return input.map(x => {
+    const distance = Math.abs(x - cell);
+
+    return static ? distance : sumOneToN(distance);
+  }).reduce((sum, x) => sum + x, 0);
 };
 
 const determineFuelConsumption = (input, static = false) => {
@@ -22,7 +28,22 @@ const determineFuelConsumption = (input, static = false) => {
   return Array.from({ length: max - min + 1 }, (_, idx) => min + idx).map((cell) => determineFuelConsumptionByIdx(input, cell, static));
 }
 
+const determineMinFuelConsumption = (input, static = false) => {
+  const fuelConsumptionArray = determineFuelConsumption(input, static);
+  const min = Math.min(...fuelConsumptionArray);
+
+  const [idx, cost] = Object.entries(fuelConsumptionArray).find(([_, cost]) => cost === min);
+
+  return { idx, cost };
+};
+
 console.log({
-  fuelArrayStatic: Math.min(...determineFuelConsumption(parsedData, true)),
-  fuelArray: Math.min(...determineFuelConsumption(parsedData)),
+  test: {
+    minFuelStatic: determineMinFuelConsumption(parsedTest, true),
+    minFuel: determineMinFuelConsumption(parsedTest),
+  },
+  data: {
+    minFuelStatic: determineMinFuelConsumption(parsedData, true),
+    minFuel: determineMinFuelConsumption(parsedData),
+  },
 });
