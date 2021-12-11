596620d9955e9af098ce8d4b9770cba309450768
Matt DuLeone <matt@duleone.com>
2021-12-10 23:25:59 -0500

    Update data fetcher to include placeholder test data array

--- getData.js
+++ getData.js
@@ -6,6 +6,14 @@ const [,, day] = process.argv;
 
 const session = process.env.session;
 
+const twoDigitNumber = num => {
+  if (num >= 10) {
+    return `${num}`;
+  }
+
+  return `0${num}`;
+};
+
 fetch(`https://adventofcode.com/2021/day/${day}/input`, {
   "headers": {
     "cookie": `session=${session}`
@@ -16,17 +24,21 @@ fetch(`https://adventofcode.com/2021/day/${day}/input`, {
 .then(data => data.split('\n').filter(el => el))
 .then(data => {
   const dataStr = `
+const test = [
+
+];
+
 const data = ${JSON.stringify(data, null, 2)};
 
-module.exports = { data };
+module.exports = { data, test };
   `.trim();
 
-  fs.writeFile(`./data/day${day}.js`, dataStr, (err) => {
+  fs.writeFile(`./data/day${twoDigitNumber(day)}.js`, dataStr, (err) => {
     if (err) {
       console.log({ err });
       return;
     }
 
-    console.log(`Fetched data for day ${day}, exported as 'data' from './data/day${day}.js'`);
+    console.log(`Fetched data for day ${day}, exported as 'data' from './data/day${twoDigitNumber(day)}.js'`);
   });
 });
